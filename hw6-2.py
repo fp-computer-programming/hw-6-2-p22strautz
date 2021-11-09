f0934ff2f511817b2215416ba2e65e5b36354ebd
p22strautz <p22strautz@fairfieldprep.org>
2021-11-09 10:20:14 -0500

    hw6-2

---
+++ hw6-2.py
@@ -0,0 +1,31 @@
+# Author: SCT (AMDG) 11/9/21
+
+# Question 1
+my_lst = [0, 1, 2, 3, 4, 5, 6, 7]
+
+# Question 2
+print(len(my_lst))
+
+# Question 3
+print(my_lst[2])
+
+# Question 4
+print(5 in [1, 2, 3])
+
+# Question 5
+print(10 in [8, 9, 10])
+
+# Question 6
+names = ['chris', 'micheal', 'mohammed', 'spencer']
+
+# Question 7
+print(names[3])
+
+# Question 8
+print(names * 3)
+
+# Question 9
+biglist = my_lst + names
+
+# Question 10
+print(len(biglist))
